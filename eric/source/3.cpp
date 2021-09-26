fc2f69924e7101c9eb7c27e90923537baa994005
ncptbtptp <13564325160@163.com>
2021-09-26 13:47:56 +0800

    Check in leetcode 3.

---
+++ eric/source/3.cpp
@@ -0,0 +1,48 @@
+/**********************************************************************************
+ * https://leetcode.com/problems/maximum-subarray/
+ *
+ * Auther: ncptbtptp
+ *
+ * Tip:
+ *
+ * Time:
+ * Space:
+ * Beat:
+  **********************************************************************************/
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.length() <= 1) return s.length();
+
+        // Direct access table
+        // "int chars[128] = {-1}" only initialize the array as {-1, 0, 0...0}
+        vector<int> chars(128, -1);
+        int maxLen = 0;
+
+        // [left, right) is the current longest substring w/o duplicate starting from s[left]
+        for (int left = 0, right = 0; right < s.length(); ++right) {
+            char next = s[right];
+            // There is a duplicate of s[right] in s[left, right)
+            if (chars[next] >= left) {
+                left = chars[next] + 1;
+            }
+            chars[next] = right;
+            maxLen = std::max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+};
+
+int main()
+{
+    vector<int> chars(128, -1);
+    cout << chars[0] << endl << chars[1] << endl;
+}
