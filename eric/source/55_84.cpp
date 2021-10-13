773bdf23b77c193726e68c50b0ae72cc215c3198
ncptbtptp <13564325160@163.com>
2021-10-13 08:53:58 +0800

    Check in leetcode 55

--- eric/source/55_84.cpp
+++ eric/source/55_84.cpp
@@ -16,6 +16,7 @@
 #include <vector>
 using namespace std;
 
+/*
 class Solution {
 public:
     bool canJump(vector<int>& nums)
@@ -53,12 +54,81 @@ public:
         return false;
     }
 };
+*/
 
-//int main()
-//{
-//    Solution so;
-//    vector<int> v{3, 2, 1, 0, 4};
-//    cout << so.canJump(v) << endl;
-//
-//    return 0;
-//}
+// recursive
+/*
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        m_memo.resize(nums.size(), -1);
+        return canJump(nums, 0);
+    }
+    
+    bool canJump(vector<int>& nums, int index) {
+        if (index >= nums.size()) return false;
+        if (index == nums.size() - 1) return true;
+        if (m_memo[index] != -1) return m_memo[index];
+        
+        bool jump(false);
+        if (index + nums[index] >= nums.size() - 1)
+            jump = true;
+        
+        for (int i = 1; !jump && i <= nums[index]; ++i) {
+            jump = canJump(nums, index + i);
+        }
+        
+        m_memo[index] = jump;
+        return jump;
+    }
+    
+private:
+    // -1: not set; 0: impossible; 1: possible
+    vector<int> m_memo;    
+};
+*/
+
+// iterative, O(n) space, O(n^2) time
+/*
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if (nums.size() == 1) return true;
+        if (nums[0] >= nums.size() - 1) return true;
+        
+        // -1: not set; 0: impossible; 1: possible
+        vector<int> memo(nums.size(), -1);
+        memo[nums.size() - 1] = 1;
+
+        for (int i = nums.size() - 2; i >= 0; --i) {            
+
+            int steps = nums[i];
+            if (i + steps >= nums.size() - 1) {
+                memo[i] = 1;
+            }
+            else {
+                int j = 1;
+                while (j <= steps && memo[i + j] == 0) ++j;
+                memo[i] = (j <= steps) ? 1 : 0;                   
+            }
+        }
+        
+        return (memo[0] == 1);
+    } 
+};
+*/
+
+// iterative, O(1) space, O(1) time
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        // next index that can jump to the last index somehow
+        int nextIndex(nums.size() - 1);
+        
+        for (int i = nums.size() - 2; i >= 0; --i) {
+            if (i + nums[i] >= nextIndex) nextIndex = i;
+        }
+        
+        return (nextIndex == 0);
+    }
+};
